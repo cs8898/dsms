29489598acfb551396239219e66faf2e964281d4
Christian Schmied <cs8898@gmx.de>
2019-04-08 15:53:13 +0200

    added minimum check for diskspace

--- dsmsMods/modules/diskspace.py
+++ dsmsMods/modules/diskspace.py
@@ -8,9 +8,13 @@ def handle(config):
     conf = json.loads(config)
     path = '/'
     scale = GIGABYTE
+    minimum = GIGABYTE
     if conf is not None:
         if "path" in conf:
             path = conf['path']
         if "scale" in conf:
             scale = conf['scale']
-    return {'ok': True, 'free': shutil.disk_usage(path).free / scale, 'endpoint': 'diskspace'}
+        if "minimum" in conf:
+            minimum = conf['minimum']*scale
+    free = shutil.disk_usage(path).free
+    return {'ok': free >= minimum, 'free': shutil.disk_usage(path).free / scale, 'endpoint': 'diskspace'}
